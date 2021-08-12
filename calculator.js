2eb857d0b6305f6caea7f39e6275a3d9f5c3e1b4
MuhammadIrfan00 <79788329+MuhammadIrfan00@users.noreply.github.com>
2021-08-12 15:31:34 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+class Calculator {
+    constructor(previousOperationandTextElement, currentoperatioandTextElement){}
+}
+
+
+
+
+const numberButtons =document.querySelectorAll('[data-number]')
+const operationButtons =document.querySelectorAll('[ data-operation]')
+const numberButtons =document.querySelectorAll('[ data-equals ]')
+const numberButtons =document.querySelectorAll('[ data-delete ]')
+const numberButtons =document.querySelectorAll('[data-all-clear]')
+const numberButtons =document.querySelectorAll('[ ]')
+const previousOperationandTextElement = document.querySelector('[data-previous-oprand]')
+const previousOperationandTextElement = document.querySelector('[data-previous-oprand]')
+const previousOperationandTextElement = document.querySelector('[data-Current-oprand]')
